445011f647dc79e39253a06bfa6c0005f1f4ccd9
Shriya Alladi <164196006+ShriyaAl@users.noreply.github.com>
2024-11-27 20:23:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from datetime import datetime
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:Chatterbox2005*@localhost/library_db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:password@localhost/library_db'
 app.config['SECRET_KEY'] = 'thisisasecretkey'
 
 db = SQLAlchemy(app)
